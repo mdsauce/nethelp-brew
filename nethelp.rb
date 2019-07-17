1bebe8d2bf21f1d826a41c0e8c3229308a1a6ca8
mdsauce <max.dobeck@saucelabs.com>
2019-07-17 13:39:17 -0700

    Updated sha. ready for 1.5.

--- nethelp.rb
+++ nethelp.rb
@@ -2,7 +2,7 @@ class Nethelp < Formula
   desc "Find out why you can't reach Sauce Lab's services. (Real Device Cloud, Virtual Cloud, Sauce Connect and more)"
   homepage "https://github.com/mdsauce/nethelp"
   url "https://github.com/mdsauce/nethelp/archive/v1.5.tar.gz"
-  sha256 ""
+  sha256 "28bd5b56d20bb54906283f423c0f505e51b06d5487fdc2d98e8857f7c8ba3ca5"
   depends_on "go" => :build
   version "1.11"
 
