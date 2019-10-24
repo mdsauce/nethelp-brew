ba6ee4cf115af6be23c68417f9c1730e2a72d799
mdsauce <max.dobeck@saucelabs.com>
2019-10-24 13:48:42 -0700

    updated nethelp brew to 1.5.1

--- nethelp.rb
+++ nethelp.rb
@@ -1,10 +1,10 @@
 class Nethelp < Formula
   desc "Find out why you can't reach Sauce Lab's services. (Real Device Cloud, Virtual Cloud, Sauce Connect and more)"
   homepage "https://github.com/mdsauce/nethelp"
-  url "https://github.com/mdsauce/nethelp/archive/v1.5.tar.gz"
-  sha256 "3460a7ea57123e710f3aea41c8d1f1893cf21c1177787d818217f9072b866cc9"
+  url "https://github.com/mdsauce/nethelp/archive/v1.5.1.tar.gz"
+  sha256 ""
   depends_on "go" => :build
-  version "1.5"
+  version "1.5.1"
 
   def install
     system "go", "build", "-o", bin/"nethelp", "."
